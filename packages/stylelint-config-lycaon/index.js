ea8c9c76789e4c706bdc006a21a1c18bed0d241c
Ben Navetta <ben.navetta@gmail.com>
2016-12-27 15:49:40 -0500

    more thorough css property listing

--- packages/stylelint-config-lycaon/index.js
+++ packages/stylelint-config-lycaon/index.js
@@ -12,14 +12,57 @@ module.exports = {
         'string-quotes': 'single',
 
         // Property order
+        // Similar to https://github.com/twitter/recess/blob/master/lib/lint/strict-property-order.js#L36
         'declaration-block-properties-order': [[
+            // Positioning
+            'position',
+            'top',
+            'bottom',
+            'left',
+            'right',
+            'z-index',
+
+            // Display
+            'display',
+            'float',
+            'width',
+            'height',
+            'max-width',
+            'max-height',
+            'min-width',
+            'min-height',
             'padding',
             'margin',
-            'border',
+            'overflow',
+            'flex',
+            'justify-content',
+            'align-items',
+
+            // Typography
             'font',
+            'line-height',
+            'letter-spacing',
+            'word-spacing',
+
+            // Visual
             'color',
+            'text',
+            'word',
+            'white-space',
+            'vertical-align',
+            'list',
+            'pointer-events',
+            'cursor',
             'background',
+            'border',
+            'content',
+            'quotes',
+            'outline',
+            'opacity',
+            'visibility',
             'transform',
+            'animation',
+            'transition'
         ], { unspecified: "bottomAlphabetical" }],
 
         // Don't use vendor prefixes since autoprefixer handles that
